40bf861ef8755ca424b6e1acae13919b77137af1
jakiir <jakir44.du@gmail.com>
2016-01-16 23:49:01 +0600

    Wp table booking - 20160116

---
+++ wp-table-bookings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WP Table Booking
+ * Plugin URI: 
+ * Description: Accept table booking and reservation online.
+ * Version: 1.0
+ * Author: Jakir Hossain
+ * Author URI: http://jakirhossain.com
+ * License:     GNU General Public License v2.0 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Text Domain: wp-table-bookings
+ * Domain Path: /languages/
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ */
+?>
